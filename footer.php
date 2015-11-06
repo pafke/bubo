1c72ffbbc69a0ae3e98df16217918cd5820074d5
Timo <timo@haragara.com>
2015-11-06 20:10:07 +0100

    v0.1

---
+++ footer.php
@@ -0,0 +1,23 @@
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/jquery.fittext.js"></script>
+    <script src="js/wow.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/creative.js"></script>
+    
+    <script>
+        $('#cycleThis li').each(function(){
+            console.log('test');
+        });
+    </script>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,211 @@
+    <?php get_header(); ?>
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <ul id="#cycleThis">
+                    <li>Text heading 1</li>
+                    <li>Dit is text heading 2</li>
+                    <li>De 3e text heading</li>
+                </ul>
+            </div>
+        </div>
+
+        <svg version="1.1" class="curved-border logo-cut" x="0px" y="0px" viewBox="0 0 189.8 64.81" enable-background="new 0 0 189.8 64.81" xml:space="preserve">
+        <g>
+            <path class="bg-primary" d="M156.645,27.139c2.885-0.298,5.859-0.595,8.645-1.076c11.186-1.884,14.92-11.269,16.118-18.38
+                C181.906,3.158,181.445,0,181.445,0s-3.422,3.845-8.205,8.019c-0.139-2.228-0.367-4.57-0.729-7.096c0,0-6.329,14.336-16.179,17.151
+                c-9.666,2.777-17.781,8.378-21.443,16.076C139.551,28.863,148.218,27.985,156.645,27.139z"/>
+            <path class="bg-primary" d="M169.951,64.753c-2.758-4.784-6.916-10.079-10.666-13.283c4.749-0.114,8.229,0.313,8.229,0.313
+                c-1.557-1.175-3.067-2.25-4.532-3.258c6.181-0.473,11.17,0.191,11.17,0.191c-1.545-1.183-3.056-2.25-4.54-3.25
+                c5.192,0.275,8.988,1.091,8.988,1.091c-2.167-3.426-4.482-6.05-6.89-8.034c0.038-0.015,0.091-0.015,0.145-0.053
+                c15.824-5.402,17.945-23.431,17.945-23.431s-1.827,1.159-4.669,2.708c-2.93,6.47-8.412,13.063-18.563,13.398
+                c-2.884,0.099-5.917-0.015-8.843-0.114c-10.35-0.351-20.139-0.702-23.945,7.233l-0.222,0.458l-0.03,0.068
+                c-0.012,0.03-0.076,0.206-0.16,0.404c-12.456-2.518-24.499-1.755-31.297-0.908c-0.053-0.007-0.103,0.016-0.164,0.016
+                c-0.198,0.03-0.396,0.038-0.583,0.068c-0.122,0.016-0.252,0.031-0.378,0.054c-0.106,0.015-0.221,0.038-0.324,0.054
+                c-1.148,0.16-2.094,0.297-2.8,0.412l-2.899,0.419c-0.698,0.092-1.656,0.229-2.785,0.412c-0.103,0.016-0.218,0.046-0.321,0.062
+                c-0.129,0.015-0.251,0.045-0.389,0.045c-0.191,0.053-0.37,0.077-0.561,0.122c-0.072,0-0.126,0.015-0.183,0.015
+                c-6.581,1.167-17.907,3.815-28.829,9.553c-6.23-8.583-17.728-13.199-30.641-10.384C18.83,41.818,9.373,30.29,9.373,30.29
+                c0.713,2.449,1.473,4.685,2.293,6.737c-6.081-1.762-10.8-3.822-10.8-3.822s1.701,5.875,6.642,11.788
+                C3.113,44.176,0,43.298,0,43.298s7.283,16.664,23.996,17.175c0.045,0.023,0.095,0.023,0.152,0.023
+                c-0.87,1.312-1.667,2.762-2.38,4.372L169.951,64.753z"/>
+        </g>
+        </svg>
+
+        <svg version="1.1" class="curved-border"  x="0px" y="0px" viewBox="0 401.6 612 11.4" style="enable-background:new 0 401.6 612 11.4;" xml:space="preserve">
+            <path class="bg-primary" d="M612,413H0v-11.4c0,0,92,11.4,306,11.4s306-11.4,306-11.4V413z"/>
+        </svg>
+
+    </header>
+	<video autoplay  poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg" id="bgvid" class="grayscale">
+	  <!-- WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
+	<source src="<?php echo get_template_directory_uri(); ?>/assets/owl.webm" type="video/webm">
+	<source src="<?php echo get_template_directory_uri(); ?>/assets/owl.mp4" type="video/mp4">
+	</video>
+
+    <section class="bg-primary"  id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2 class="section-heading text-primary ">What we do?</h2>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-justify">
+                    <div class="service-box">
+					    <h3>Strategy</h3>
+                        <i class="fa fa-4x fa-diamond wow bounceIn text-primary"></i>
+                        <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-justify">
+                    <div class="service-box">
+					    <h3>Branding</h3>
+                        <i class="fa fa-4x fa-paper-plane wow bounceIn text-primary" data-wow-delay=".1s"></i>
+                        <p>T. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-justify">
+                    <div class="service-box">
+						<h3>Concepting</h3>
+                        <i class="fa fa-4x fa-newspaper-o wow bounceIn text-primary" data-wow-delay=".2s"></i>
+                        <p>This is in gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-justify">
+                    <div class="service-box">
+						<h3>Developing</h3>
+                        <i class="fa fa-4x fa-heart wow bounceIn text-primary" data-wow-delay=".3s"></i>
+                        <p>This. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="no-padding bg-primary" id="portfolio">
+		<div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2 class="section-heading text-primary ">How we do it?</h2>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row no-gutter">
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/portfolio/1.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/portfolio/2.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/portfolio/3.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/portfolio/4.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/portfolio/5.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/portfolio/6.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-primary" id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Let's Get In Touch!</h2>
+                    <hr class="primary">
+                    <p>Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+                </div>
+                <div class="col-lg-4 col-lg-offset-2 text-center">
+                    <i class="fa fa-phone fa-3x wow bounceIn"></i>
+                    <p>123-456-6789</p>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
+                    <p><a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php get_footer(); ?>
