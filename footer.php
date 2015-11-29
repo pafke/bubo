84d4d73552d30102bd4c4aae3bd1e6b9417ef899
Timo <timo@haragara.com>
2015-11-29 19:57:54 +0100

    video height ingekort en single page template aangemaakt

--- footer.php
+++ footer.php
@@ -1,4 +1,56 @@
-    <!-- Bootstrap Core JavaScript -->
+    <section class="bg-primary" id="contact">        
+        <svg version="1.1" class="logo-cut-bottom" x="0px" y="0px" viewBox="0 291.512 612 209.163" enable-background="new 0 0 189.8 64.81" xml:space="preserve">
+            <g>
+                <path class="bg-primary" stroke="#FFF" stroke-width="3" d="M505.093,379.02c9.303-0.961,18.893-1.918,27.876-3.469c36.068-6.075,48.108-36.336,51.971-59.266
+                    c1.606-14.59,0.12-24.773,0.12-24.773s-11.034,12.398-26.457,25.857c-0.448-7.184-1.184-14.736-2.351-22.881
+                    c0,0-20.407,46.226-52.168,55.303c-31.168,8.954-57.334,27.014-69.142,51.836C449.975,384.579,477.921,381.748,505.093,379.02z"/>
+                <path class="bg-primary" stroke="#FFF" stroke-width="3" d="M547.997,500.305c-8.893-15.426-22.3-32.499-34.392-42.83c15.313-0.368,26.534,1.009,26.534,1.009
+                    c-5.021-3.789-9.89-7.255-14.613-10.505c19.931-1.525,36.017,0.615,36.017,0.615c-4.981-3.814-9.854-7.255-14.639-10.479
+                    c16.741,0.887,28.981,3.519,28.981,3.519c-6.987-11.047-14.452-19.508-22.217-25.906c0.123-0.048,0.294-0.048,0.468-0.17
+                    C605.16,398.138,612,340.004,612,340.004s-5.892,3.737-15.056,8.732c-9.447,20.862-27.124,42.121-59.855,43.201
+                    c-9.299,0.319-19.079-0.048-28.514-0.367c-33.373-1.132-64.937-2.264-77.209,23.323l-0.716,1.477l-0.097,0.219
+                    c-0.039,0.098-0.245,0.664-0.517,1.303c-40.163-8.119-78.995-5.658-100.915-2.928c-0.171-0.022-0.332,0.052-0.529,0.052
+                    c-0.638,0.097-1.276,0.122-1.88,0.22c-0.393,0.051-0.813,0.1-1.219,0.174c-0.342,0.049-0.712,0.122-1.044,0.174
+                    c-3.702,0.516-6.752,0.957-9.029,1.328l-9.348,1.352c-2.25,0.297-5.339,0.738-8.979,1.328c-0.332,0.052-0.703,0.148-1.035,0.2
+                    c-0.416,0.048-0.81,0.146-1.254,0.146c-0.616,0.17-1.193,0.248-1.81,0.393c-0.231,0-0.406,0.049-0.59,0.049
+                    c-21.22,3.763-57.74,12.301-92.957,30.803c-20.089-27.675-57.163-42.559-98.801-33.482c-39.932,8.654-70.425-28.517-70.425-28.517
+                    c2.299,7.897,4.749,15.107,7.394,21.723C18.008,405.223,2.792,398.58,2.792,398.58s5.485,18.943,21.417,38.01
+                    C10.037,433.955,0,431.124,0,431.124s23.483,53.732,77.374,55.38c0.145,0.074,0.306,0.074,0.489,0.074
+                    c-2.805,4.23-5.375,8.906-7.674,14.097"/>
+            </g>
+        </svg>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 col-sm-3">
+                    <h4>Adress</h3>
+                    Torenallee 62,22<br>
+                    5617 BD, Eindhoven
+                </div>
+                <div class="col-lg-2 col-sm-3">
+                    <h4>Contact Thijs:</h3>
+                    <a href="mailto:thijs@bubobubo.eu"><span>thijs</span>@bubobubo.eu</a>
+                    <span>+</span>31 6 427 69 996
+                </div>
+                <div class="col-lg-2 col-sm-3">
+                    <h4>Contact Ben:</h3>
+                    <a href="mailto:ben@bubobubo.eu"><span>ben</span>@bubobubo.eu</a>
+                    <span>+</span>31 6 427 69 997
+                </div>
+                <div class="col-lg-6 col-sm-12">
+                    <div class="container-about">
+                        <div class="container-img">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/logo_vert.svg">
+                        </div>
+                        <div class="container-content">
+                            <h4>About Bubo Bubo</h3> 
+                            This is Photoshop's version  of Lorem Ipsum. Proin  urna neque. Suspendisse in orci enim.Let's inspire each other with great stories from all over the world!                    
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
 
     <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
---
+++ single.php
@@ -0,0 +1,10 @@
+<?php
+
+get_header(); ?>
+
+    <section class="bg-portfolio" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/bg_burger.png');">
+        <div class="container">
+        </div>
+    </section>
+    
+<?php get_footer(); ?>
