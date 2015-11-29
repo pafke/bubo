96d0d31bddbd1859069994eea73db99609ee1619
Timo <timo@haragara.com>
2015-11-29 16:27:53 +0100

    thumbnails uploadbaar gemaakt

--- functions.php
+++ functions.php
@@ -5,9 +5,28 @@ function bubo_scripts() {
     wp_enqueue_script( 'jQuery', 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', array(), '1.0.0', false );
 }
 
+add_image_size( 'logo_size', 140, 100);
+
 add_action( 'wp_enqueue_scripts', 'bubo_scripts' );
+
+add_theme_support('post-thumbnails');
 
 function register_my_menu() {
     register_nav_menu('header-menu',__( 'Header Menu' ));
 }
 add_action( 'init', 'register_my_menu' );
+
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+  register_post_type( 'portfolio_item',
+    array(
+      'labels' => array(
+        'name' => __( 'Portfolio items' ),
+        'singular_name' => __( 'Portfolio item' )
+      ),
+      'supports' => array( 'title', 'editor', 'thumbnail',),
+      'public' => true,
+      'has_archive' => true,
+    )
+  );
+}
--- index.php
+++ index.php
@@ -126,6 +126,25 @@
                         <img src="<?php echo get_template_directory_uri(); ?>/img/finnik_logo.jpg" alt="">
                     </div>
                 </a>
+                <?php
+                $args = array( 'post_type' => 'portfolio_item');
+                $loop = new WP_Query( $args );
+                while ( $loop->have_posts() ) : $loop->the_post();
+                ?>
+                <a href="<?php the_permalink(); ?>" class="portfolio-link">
+                    <div class="col-lg-3 col-sm-6 portfolio-item">
+                        <?php 
+                        $image = get_field('logo');
+                        $size = 'logo_size';
+                        $thumb = $image['sizes'][ $size ];
+                        if( !empty($image) ): ?>
+                            <img src="<?php echo $thumb; ?>" alt="<?php echo $image['alt']; ?>" />
+                        <?php endif; ?>
+                    </div>
+                </a>
+                <?php
+                endwhile;
+                ?>
             </div>
         </div>
         
